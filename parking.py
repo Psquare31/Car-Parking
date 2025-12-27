da6cd86e4ea07499d0ad58464e68cb4c4543bc90
Psquare <pranavprajyot31@gmail.com>
2025-12-27 15:54:01 +0530

    First object

--- parking.py
+++ parking.py
@@ -1,3 +1,6 @@
-class Parking():
-    def __init__(self):
-        pass
+class Car:
+    def __init__(self,car_number,car_type,entry_time):
+        self.car_number=car_number
+        self.car_type=car_type
+        self.entry_time=entry_time
+    
